0ded9638bb7f573af827adc75934fd69fbda6936
f4ww4z <fawwazyusran@gmail.com>
2020-07-16 20:58:12 +0800

    Create model serializers
    - Test users list API view

---
+++ main/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.state import User
+
+from main.models import Category, Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
