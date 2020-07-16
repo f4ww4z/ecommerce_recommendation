6c69b9084c6314c5ceb95f8d9cd41f2a423987a0
f4ww4z <fawwazyusran@gmail.com>
2020-07-16 22:26:55 +0800

    Add Live Swagger docs

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,26 @@
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
 from .views import UserList
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Recommendation-based E-Commerce app",
+        default_version='v1',
+        description='A sample e-commerce web app backend',
+        contact=openapi.Contact(email='faww4zintelgent4@gmail.com'),
+        license=openapi.License(name='MIT License'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path('swagger.json', schema_view.without_ui(cache_timeout=0),
+         name='schema-json'),
+    path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('users', UserList.as_view(), name='users-list'),
 ]
