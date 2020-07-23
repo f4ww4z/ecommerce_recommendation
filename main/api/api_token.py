1794b6c5054b53ee66be667285199a787c3cea1c
f4ww4z <fawwazyusran@gmail.com>
2020-07-23 23:44:50 +0800

    Fix refresh token endpoint

--- main/api/api_token.py
+++ main/api/api_token.py
@@ -1,10 +1,7 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from main import serializers
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = serializers.MyTokenObtainPairSerializer
-
-
-class MyTokenRefreshView(TokenRefreshView):
     serializer_class = serializers.MyTokenObtainPairSerializer
